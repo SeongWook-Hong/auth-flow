04a0794046a724b1296a3e9cff113ab2f89b3bf0
wook <hsw5971@naver.com>
2025-06-27 16:05:12 +0900

    fix: 입력값이 존재할 때, label 겹침 문제 수정

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface Props {
   id: "email" | "password" | "confirmPassword";
   type: "email" | "password";
@@ -5,15 +7,29 @@ interface Props {
 }
 
 const AuthInput = ({ id, type, label }: Props) => {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const isFloating = value.length > 0 || isFocused;
   return (
     <div
       tabIndex={0}
       className="relative flex flex-col border px-4 pt-6 pb-2 rounded-md focus-within:ring-2 focus-within:ring-blue-500"
     >
-      <input className="peer focus:outline-none" id={id} type={type} />
+      <input
+        className="peer focus:outline-none"
+        id={id}
+        type={type}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
       <label
-        className="absolute left-4 top-4 text-m text-gray-500 transition-all peer-focus:top-1 peer-focus:text-sm peer-focus:text-blue-500"
         htmlFor={id}
+        className={`absolute left-4 transition-all text-gray-500 ${
+          isFloating ? "top-1 text-sm" : "top-4 text-base"
+        }`}
       >
         {label}
       </label>
